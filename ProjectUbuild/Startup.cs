06dc64ee0d33d7eb10389d87359ba3d63127b8ce
NanaSafo <brudexgh@gmail.com>
2018-09-01 13:42:05 +0000

    iniitial commit

---
+++ ProjectUbuild/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectUbuild.Startup))]
+namespace ProjectUbuild
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

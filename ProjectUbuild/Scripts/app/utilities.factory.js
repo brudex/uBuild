111e47ca13c33a040aece15d166414512ac2f35b
nana safo <brudexgh@gmail.com>
2018-09-23 14:44:49 +0000

    utils

---
+++ ProjectUbuild/Scripts/app/utilities.factory.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    angular
+        .module('ubuild')
+        .factory('brudexutils', UtilityFunctions);
+    UtilityFunctions.$inject = ['$http', '$location', '$window'];
+    function UtilityFunctions($http, $location,$window) {
+         return {
+            alertSuccess: createAlert('success'),
+            alertError: createAlert('error')
+           
+        }; 
+        function createAlert(alertType) {
+            return function showAlert(title, message) {
+                var args = [].slice.call(arguments);
+                if (args.length === 1) {
+                    message = args[0];
+                    title = "Success";
+                }
+                $window.swal(title, message, alertType);
+            } 
+        }
+        
+
+    }
+})();

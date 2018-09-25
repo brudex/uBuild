38adf81a19f19a811783949883eed97c7fd60879
nana safo <brudexgh@gmail.com>
2018-09-25 21:41:03 +0000

    loan application

---
+++ uBuildCore/AccountProfileHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GHLService;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using uBuildCore.Models;
+
+namespace uBuildCore
+{
+     public class AccountProfileHandler
+    {
+         public static ServiceResponse GetAccountProfile(ClientAuths client)
+         {
+             T24Customer tclient = null;
+            try
+             {
+                
+                 tclient = T24Customer.GetCustomerBYNumber(client.MobileNumber.FormatMobile());
+             }
+             catch (Exception ex)
+             {
+                 Logger.Error(typeof(AccountProfileHandler), "Errror in T24Customer.GetCustomerBYNumber ",ex);
+             }        
+            var dataResult = new JObject();
+            dataResult["fullName"] = client.FullName();
+            if (tclient != null)
+            { 
+                dataResult["customerNo"] = tclient.CustomerNo;
+                dataResult["accountNumber"] = tclient.CustomerNo;
+            } 
+            var response = new ServiceResponse();
+            response.Status = "00";
+            response.data = dataResult;
+            return response;
+        }
+
+
+    }
+}

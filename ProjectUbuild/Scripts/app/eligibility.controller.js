7edb8b9ef6bbfc8b455dabf4a153591c8686c637
Philip <darkwaphil@gmail.com>
2018-09-26 11:26:00 +0000

    complete elibility

--- ProjectUbuild/Scripts/app/eligibility.controller.js
+++ ProjectUbuild/Scripts/app/eligibility.controller.js
@@ -4,8 +4,8 @@
     'use strict';
     angular.module("ubuild")
         .controller("EligibilityCtrl", EligibilityCtrl);
-    EligibilityCtrl.$inject = [ "$scope", "$http", "$timeout", "$rootScope",'brudexservices', 'brudexutils'];
-    function EligibilityCtrl($scope, $http, $timeout, $rootScope, services, utils) {
+    EligibilityCtrl.$inject = ["$scope", "$http", "$timeout", "$rootScope", 'brudexservices', 'brudexutils', '$window'];
+    function EligibilityCtrl($scope, $http, $timeout, $rootScope, services, utils, $window) {
         var isLoanApplication = false;
         var callbackFunc = null;
         $scope.init = function (isLoanApp, callback) {
@@ -13,122 +13,142 @@
                 isLoanApplication = isLoanApp;
                 callbackFunc = callback;
             }
-            
+
             $scope.builder = {
-                    loanType: null,
-                    phase: ""
-                }; 
-            }; 
-            $scope.$watch("builder.loanType",
-                function (loantype) {
-                    console.log("$scope.builder.loanType", loantype);
-                });
-            $scope.$watch("builder.loanAmount",
-                function (loanAmount) {
-                    console.log("$scope.builder.loanAmount", loanAmount);
-                });
+                loanType: null,
+                phase: ""
+            };
+        };
+        $scope.$watch("builder.loanType",
+            function (loantype) {
+                console.log("$scope.builder.loanType", loantype);
+            });
+        $scope.$watch("builder.loanAmount",
+            function (loanAmount) {
+                console.log("$scope.builder.loanAmount", loanAmount);
+            });
 
-            $scope.$watch("builder.phase",
-                function (phase) {
-                    console.log("$scope.builder.phase", phase);
-                });
+        $scope.$watch("builder.phase",
+            function (phase) {
+                console.log("$scope.builder.phase", phase);
+            });
 
-            $scope.$watch("builder.currency",
-                function (currency) {
-                    if (typeof currency != 'undefined') {
+        $scope.$watch("builder.currency",
+            function (currency) {
+                if (typeof currency != 'undefined') {
 
-                        //slider options
-                        var monthlyIncomeOptions = {
-                            min: 1000,
-                            max: 100000,
-                            type: 'single',
-                            step: 100,
-                            prefix: currency,
-                            maxPostfix: "+",
-                            prettify_enabled: false,
-                            grid: true
-                        }
-
-                        var loanAmountSliderOptions = {
-                            min: 1000,
-                            max: 100000,
-                            type: 'single',
-                            step: 100,
-                            prefix: currency,
-                            maxPostfix: "+",
-                            prettify_enabled: false,
-                            grid: true
-                        }
-
-                        var loanRateSliderOptions = {
-                            min: 1,
-                            max: 100,
-                            type: 'single',
-                            step: 1,
-                            from: 12,
-                            disable: true,
-                            postfix: "%",
-                            maxPostfix: "+",
-                            prettify_enabled: true,
-                            grid: false
-                        }
-
-                        var loanTenureSliderOptions = {
-                            min: 1,
-                            max: 20,
-                            from: 12,
-                            type: 'single',
-                            step: 1,
-                            postfix: "years",
-                            maxPostfix: "+",
-                            prettify_enabled: true,
-
-                        }
-                        //instantiate sliders
-                        $("#loan_monthly_income").ionRangeSlider();
-                        $("#loan_amount").ionRangeSlider();
-                        $("#loan_Irate").ionRangeSlider();
-                        $("#loan_Tenure").ionRangeSlider();
-
-                        //get slider instances
-                        var monthlyIncomeSlider = $("#loan_monthly_income").data("ionRangeSlider");
-                        var loanAmountSlider = $("#loan_amount").data("ionRangeSlider");
-                        var loanRateSlider = $("#loan_Irate").data("ionRangeSlider");
-                        var loanTenureSlider = $("#loan_Tenure").data("ionRangeSlider");
-                        monthlyIncomeSlider.update(monthlyIncomeOptions);
-                        loanAmountSlider.update(loanAmountSliderOptions);
-                        loanRateSlider.update(loanRateSliderOptions);
-                        loanTenureSlider.update(loanTenureSliderOptions);
+                    //slider options
+                    var monthlyIncomeOptions = {
+                        min: 1000,
+                        max: 100000,
+                        type: 'single',
+                        step: 100,
+                        prefix: currency,
+                        maxPostfix: "+",
+                        prettify_enabled: false,
+                        grid: true
                     }
-                });
 
-            $scope.checkEligibility = function () {
-                var payload = Object.assign({}, $scope.builder);
-                console.log('the payload is >>>', payload);
-                services.checkLoanEligibility(payload, function (response) {
-                    console.log("the response for eligibility >>", response);
-                    if (response.Status === "00") {
-                        utils.alertSuccess("Congratulations",response.Message);
-                    }else if (response.Status === "01") {
-                        utils.alertError("Sorry",response.Message);
+                    var loanAmountSliderOptions = {
+                        min: 1000,
+                        max: 100000,
+                        type: 'single',
+                        step: 100,
+                        prefix: currency,
+                        maxPostfix: "+",
+                        prettify_enabled: false,
+                        grid: true
                     }
-                    if (isLoanApplication && callbackFunc) {
-                        var obj = { request: payload, response: response };
-                        callbackFunc(obj);
+
+                    var loanRateSliderOptions = {
+                        min: 1,
+                        max: 100,
+                        type: 'single',
+                        step: 1,
+                        from: 12,
+                        disable: true,
+                        postfix: "%",
+                        maxPostfix: "+",
+                        prettify_enabled: true,
+                        grid: false
                     }
-                }); 
-            }
-            
 
+                    var loanTenureSliderOptions = {
+                        min: 1,
+                        max: 20,
+                        from: 12,
+                        type: 'single',
+                        step: 1,
+                        postfix: "years",
+                        maxPostfix: "+",
+                        prettify_enabled: true,
 
+                    }
+                    //instantiate sliders
+                    $("#loan_monthly_income").ionRangeSlider();
+                    $("#loan_amount").ionRangeSlider();
+                    $("#loan_Irate").ionRangeSlider();
+                    $("#loan_Tenure").ionRangeSlider();
 
+                    //get slider instances
+                    var monthlyIncomeSlider = $("#loan_monthly_income").data("ionRangeSlider");
+                    var loanAmountSlider = $("#loan_amount").data("ionRangeSlider");
+                    var loanRateSlider = $("#loan_Irate").data("ionRangeSlider");
+                    var loanTenureSlider = $("#loan_Tenure").data("ionRangeSlider");
+                    monthlyIncomeSlider.update(monthlyIncomeOptions);
+                    loanAmountSlider.update(loanAmountSliderOptions);
+                    loanRateSlider.update(loanRateSliderOptions);
+                    loanTenureSlider.update(loanTenureSliderOptions);
+                }
+            });
 
+        $scope.checkEligibility = function () {
+            var payload = Object.assign({}, $scope.builder);
+            console.log('the payload is >>>', payload);
+            services.checkLoanEligibility(payload, function (response) {
+                console.log("the response for eligibility >>", response);
+                if (response.Status === "00") {
 
+                    swal({
+                        title: "Congratulations",
+                        text: response.Message,
+                        buttons: {
+                            cancel: "OK",
+                            catch: {
+                                text: "Apply Now",
+                                value: "applyButton"
+                            }
+                        }
+                    })
+                        .then((value) => {
+                            switch (value) {
+                                case "applyButton":
+                                    var newUrl = window.location.host + "/loan/apply";
+                                    console.log("newUrl", newUrl);
+                                    $window.location.href = window.location.host + "/loan/apply";
+                                    break;
+                            }
+                        });
+                } else if (response.Status === "01") {
+                    utils.alertError("Sorry", response.Message);
+                }
+                if (isLoanApplication && callbackFunc) {
+                    var obj = { request: payload, response: response };
+                    callbackFunc(obj);
+                }
+            });
         }
-   
 
-     
+
+
+
+
+
+    }
+
+
+
 })();
 
- 
+
 

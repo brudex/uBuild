b7aea155fb303cecafb2bfcc87eb8ddab991bdf6
nana safo <brudexgh@gmail.com>
2018-09-30 16:39:42 +0000

    fittings and fixtures

---
+++ uBuildCore/Models/FixturesAndFittings.cs
@@ -0,0 +1,14 @@
+namespace uBuildCore.Models
+{
+    public class FixturesAndFittings
+    { 
+    public int RecordId { get; set; }
+        public string ItemDescription { get; set; }
+        public int CustomizableFnFsId { get; set; }
+        public decimal UnitCost { get; set; }
+        public int CostCurrencyId { get; set; }
+        public string ItemImage { get; set; }  
+
+
+    }
+}

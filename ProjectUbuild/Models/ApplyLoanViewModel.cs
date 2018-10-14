30ec4256a2840c9710e4449c1ed2526a10698e8d
nana safo <brudexgh@gmail.com>
2018-10-14 15:35:56 +0000

    commited

---
+++ ProjectUbuild/Models/ApplyLoanViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectUbuild.Models
+{
+    public class ApplyLoanViewModel
+    {
+        public string loanType { get; set; }
+        public decimal monthlyIncome { get; set; }
+        public decimal loanAmount { get; set; }
+        public string currency { get; set; }
+        public int loanTenure { get; set; }
+        public string json { get; set; }
+    }
+    
+}

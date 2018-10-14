0829a187d7f36934d6fea3997bce162bd9e8e232
nana safo <brudexgh@gmail.com>
2018-10-14 21:33:09 +0000

    building phases

---
+++ uBuildCore/Models/BuildingPhases.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uBuildCore.Models
+{
+    public class BuildingPhases
+    {
+       public int  RecordId{ get; set; }
+      public  string PhaseName{ get; set; }
+    }
+}

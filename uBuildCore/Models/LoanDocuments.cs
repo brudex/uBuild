8d7db9a40c6c2a3eae2875d84abb65effda94d00
Nana Safo <brudexgh@gmail.com>
2018-10-21 20:05:47 +0000

    loan docs

---
+++ uBuildCore/Models/LoanDocuments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uBuildCore.Models
+{
+   public class LoanDocuments
+    {
+
+      public int RecordId{get;set;}
+      public string ULAIN{get;set;}
+      public int DocTypeId{get;set;}
+      public string Description{get;set;}
+      public string DocumentPath{get;set;}
+      public string DateUploaded{get;set;}
+      public int CreatorId{get;set;}
+      public string CreatorName{get;set;}
+      public DateTime CreatedDate{get;set;}
+    }
+}
---
+++ uBuildCore/Models/LoanProcessStages.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace uBuildCore.Models
+{
+    public class LoanProcessStages
+    {
+      public int RecordId{get;set;}
+	  public string CustomerNo{get;set;}
+      public string ULAIN{get;set;}
+      public int LoanApplTypeId{get;set;}
+      public int BuildingPhaseId{get;set;}
+      public string PurposeofLoan{get;set;}
+      public decimal AmtSought{get;set;}
+      public int CurrencyId{get;set;}
+      public string CurrencyCode{get;set;}
+      public int LoanTermMonths{get;set;}
+      public int RepaymentMethodId{get;set;}
+      public string RepaymentMethod { get; set; }
+      public bool ProtectionCover{get;set;}
+      public bool ProtectionSecured{get;set;}
+      public string ProtectionSecurityType{get;set;}
+      public string ProtectionSecurityDetails{get;set;}
+      public bool ApplSubmitted{get;set;}
+      public DateTime ApplSubmitDate{get;set;}
+	  public int LPS01InitialReview{get;set;}
+      public int LPS02CreditAssessment{get;set;}
+      public int LPS03RiskAssessment{get;set;}
+      public int LPS04CreditApproval{get;set;}
+      public int LPS05ClientConfirmation{get;set;}
+      public int LPS06LoanDisbursement{get;set;}
+      public string ProcessComment{get;set;}
+      public DateTime LastProcessDate{get;set;}
+
+    }
+}

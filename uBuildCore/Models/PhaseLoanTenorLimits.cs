21676be02878404db032a34b720b1b9dcb0ca284
nana safo <brudexgh@gmail.com>
2018-11-05 00:30:14 +0000

    eligibility fix

---
+++ uBuildCore/Models/PhaseLoanTenorLimits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uBuildCore.Models
+{
+   public class PhaseLoanTenorLimits
+    {
+      public int RecordId{ get; set; }
+      public int ForPhase{ get; set; }
+      public int MinTenorMonths{ get; set; }
+      public int MaxTenorMonths{ get; set; }
+    }
+}

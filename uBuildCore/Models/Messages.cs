78b12b1c105307b667d35b808d2f3eecfa9db7fa
nana safo <brudexgh@gmail.com>
2019-03-04 07:50:12 +0000

    commit

---
+++ uBuildCore/Models/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uBuildCore.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Receipient { get; set; }
+        public string Message { get; set; }
+        public DateTime  CreatedAt { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
